171843af780aa3a0890c78216f0c9733eea5f806
Seanbon0611 <seand0611@gmail.com>
2020-05-12 17:20:34 -0700

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+  attr_reader :name, :weight, :height, :ships
+  @@all = []
+  
+  def initialize(args)
+      @name = args[:name]
+      @weight = args[:weight]
+      @height = args[:height]
+      @ships = args[:ships]
+      @@all << self
+  end
+
+
+
+
+
+  def self.all
+      @@all
+  end
+
+
 end
